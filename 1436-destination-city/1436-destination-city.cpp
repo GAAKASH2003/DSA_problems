e844553fa604503950ca62bbdd222c8031368297
AAKASH751 <103364875+GAAKASH2003@users.noreply.github.com>
2023-12-15 09:27:15 +0530

    Time: 10 ms (62.92%), Space: 10.9 MB (71.53%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        set<string>s;
+        unordered_set<string>s;
         int n=paths.size();
         for(int i=0;i<n;i++){
             s.insert(paths[i][1]);
