b1c9b04fbbb593f7295b67773b04ed8f0e33582b
AAKASH751 <103364875+GAAKASH2003@users.noreply.github.com>
2023-12-28 11:58:32 +0530

    Time: 3 ms (77.32%), Space: 7.5 MB (79.00%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -6,8 +6,8 @@ public:
        stack<int>st;
         st.push(-1);
         int ma=0;
-        int n=s.length();
-        for(int i=0;i<n;i++){
+        // int n=;
+        for(int i=0;i<s.length();i++){
             if(s[i]=='('){
                 st.push(i);
             }
